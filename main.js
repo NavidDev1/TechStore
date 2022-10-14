1b0f61bf5f88b0e68c622d45542195df6204e5b1
Casper Werner <cappe1996@hotmail.com>
2022-10-14 14:48:54 +0200

    la till lite förklaring i koden med comments

--- main.js
+++ main.js
@@ -20,9 +20,13 @@ function initSite() {
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
     let containerOfPhones = document.querySelector(".containerOfPhones");
+    /************let Output för att vi vill ange ett värde som vi sen ska deklarera in i vår main page (containerOfPhones)*********/
     let output = "";
+    /************Vi loopar alla produkter från JSON********/
     for (const product of listOfProducts){
-    output += `
+    /*****Vi anger += för att ge outputen flera värden då vi loopar så länge produkter finns*******/
+    /******************BACKTICKS SÅ VI KAN SKRIVA HTMLKOD OCH LÄGGA IN VÅRA PRODUKTER I ELEMENTEN PÅ ETT SMIDIGT SÄTT****************************/
+    output += `  
         <div class="ofItems">
             <h1>${product.title}</h1>
             <p>${product.description}</p>
@@ -32,5 +36,6 @@ function addProductsToWebpage() {
         </div>
     `
     }
+    /**************EFTER ATT LOOPEN HAR GÅTT IGENOM SÅ LÄGGER VI IN INFORMATIONEN I CONTAINER*********/
     containerOfPhones.innerHTML = output;
 }
