b0c96256e0f256eeeed0778a8911c18a5b2803b3
Raul B <r.au.l@outlook.com>
2022-10-17 08:51:01 +0200

    cart logic, display shopping cart in js. css wip

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+const phones = {
+	iphonex: 0,
+	oneplus5: 1,
+	galaxys8: 2,
+	lgv30: 3
+};
+const navShoppingCartBtn =  document.getElementById("shoppingCartBtn");
+
+let containerOfPhones = document.querySelector(".containerOfPhones");
 let customerId;
 let addToShoppingCartBtns;
 let shoppingList = [];
 let numberOfItemsInShoppingList;
 var listOfProducts;
 let customer = {};
+
+
 /** Get products from the json file and store it in a gobal variable */
 function loadProducts() {
     fetch("./products.json")
@@ -30,13 +41,11 @@ function loadProducts() {
     });
 }
 
-
 function initSite() {
     loadProducts();
     // This would also be a good place to initialize other parts of the UI
 }
 
-let containerOfPhones = document.querySelector(".containerOfPhones");
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
     
@@ -69,11 +78,58 @@ function addPutToShoppingCartBtnListners(){
 }
 
 function addToShoppingCart(){
-    customer.shoppingList.push(this.id);
+    switch (this.id) {
+        case "iphonex":
+            customer.shoppingList.push(listOfProducts[phones.iphonex]);
+            break;
+        case "oneplus5":
+            customer.shoppingList.push(listOfProducts[phones.oneplus5]);
+            break;
+        case "galaxys8":
+            customer.shoppingList.push(listOfProducts[phones.galaxys8]);
+            break;
+        case "lgv30":
+            customer.shoppingList.push(listOfProducts[phones.lgv30]);
+            break;    
+        default:
+            console.log("No such product found to add to the shopping list!");
+            break;
+    }
     numberOfItemsInShoppingList = customer.shoppingList.length;
     window.localStorage.setItem('activeCustomer', JSON.stringify(customer));
     console.log(`Number of items in shopping list after adding to basket is: ${numberOfItemsInShoppingList}`);
     console.log(customer.shoppingList);
+    createUlFromShoppingCartList();
 }
 
+function displayShoppingCart(){
+    let list = createUlFromShoppingCartList()
+    containerOfPhones.replaceChildren(list);
+    containerOfPhones.className = "containerOfShoppingCart";
+    document.querySelector(".ofItems").style.height = "50vh";
 
+}
+
+function createUlFromShoppingCartList(){
+    let list = document.createElement('ul');
+    for(const phone of customer.shoppingList){
+        item = document.createElement('li');
+        item.innerHTML = createItemsDiv(phone);
+        list.appendChild(item);
+    }
+    console.log(list);
+    return list;
+}
+
+function createItemsDiv(item){
+    let title = item.title
+    let btnId = `btnRemove${title.split(" ").join("").toLowerCase()}`
+    return `  
+        <div class="cartItem">            
+            <img src = "assets/${item.image}"></img>
+            <h1>${item.title}</h1>
+            <h2>${item.price}</h2>
+            <button id=${btnId} class="removeFromShoppingCartBtn">Ta bort</button>
+        </div>
+    `;   
+}
