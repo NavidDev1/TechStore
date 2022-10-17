b49ec2bb98870443da241e2a9d9e5a5656c3d111
r-4u-1 <114141854+r-4u-1@users.noreply.github.com>
2022-10-17 10:57:56 +0200

    Update main.js

--- main.js
+++ main.js
@@ -20,25 +20,6 @@ function initSite() {
 let containerOfPhones = document.querySelector(".containerOfPhones");
 /** Uses the loaded products data to create a visible product list on the website */
 function addProductsToWebpage() {
-    
-    /************let Output för att vi vill ange ett värde som vi sen ska deklarera in i vår main page (containerOfPhones)*********/
-    let output = "";
-    /************Vi loopar alla produkter från JSON********/
-    for (const product of listOfProducts){
-    /*****Vi anger += för att ge outputen flera värden då vi loopar så länge produkter finns*******/
-    /******************BACKTICKS SÅ VI KAN SKRIVA HTMLKOD OCH LÄGGA IN VÅRA PRODUKTER I ELEMENTEN PÅ ETT SMIDIGT SÄTT****************************/
-    output += `  
-        <div class="ofItems">
-            <h1>${product.title}</h1>
-            <p>${product.description}</p>
-            <img src = "assets/${product.image}"></img>
-            <h2>${product.price}</h2>
-            <button>Lägg till i kundvagnen</button>
-        </div>
-    `
-    }
-    /**************EFTER ATT LOOPEN HAR GÅTT IGENOM SÅ LÄGGER VI IN INFORMATIONEN I CONTAINER*********/
-    containerOfPhones.innerHTML = output;
 }
 
 console.log(containerOfPhones);
